21f7e5fcb9efee2d2e9ae00d83cde684085a3e3c
luiz.philipe <luizp4979@gmail.com>
2024-07-28 12:52:45 -0300

    Melhorando tela Home e adicionando FlatList

--- example-android.ps1
+++ example-android.ps1
@@ -1,6 +1,6 @@
 $originalDir = Get-Location
 Set-Location -Path '' # Caminho para a pasta emulator, geralmente em 'Appdata\Local\Android\Sdk\emulator\'ou 'Appdata\Local\Android\Sdk\tools\emulator\'
-Start-Process -FilePath './emulator' -ArgumentList '-avd S20' -NoNewWindow
+Start-Process -FilePath './emulator' -ArgumentList '-avd [AVD NAME]' -NoNewWindow # Troque [AVD NAME] pelo nome da sua AVD, você pode chegcar executando o comando '/.emulator --list-avds'
 Start-Sleep -Seconds 10
 Set-Location -Path $originalDir
 expo start --android
